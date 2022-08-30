ef7d73319901665aa21bd252e7a659843637359f
jhahn <jhahn@twave.co.kr>
2022-08-30 18:23:25 +0900

    Spring Batch Paging Reader 사용시 같은 조건의 데이터를 읽고 수정해결 [진행중]

--- src/main/java/kr/co/imin/batch/job/itemReader/PayPagingFailJobConfiguration.java
+++ src/main/java/kr/co/imin/batch/job/itemReader/PayPagingFailJobConfiguration.java
@@ -1,12 +1,15 @@
 package kr.co.imin.batch.job.itemReader;
 
 import kr.co.imin.batch.entity.PayTest;
+import kr.co.imin.batch.repository.PayRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
@@ -14,8 +17,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
 
 import javax.persistence.EntityManagerFactory;
+
+import java.util.Collections;
 
 import static kr.co.imin.batch.job.itemReader.PayPagingFailJobConfiguration.JOB_NAME;
 
@@ -32,6 +38,7 @@ public class PayPagingFailJobConfiguration {
     private final EntityManagerFactory entityManagerFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final JobBuilderFactory jobBuilderFactory;
+    private final PayRepository payRepository;
 
     private final int chunkSize = 10;
 
@@ -53,16 +60,53 @@ public class PayPagingFailJobConfiguration {
                 .build();
     }
 
+    //실패 코드
+//    @Bean
+//    @StepScope
+//    public JpaPagingItemReader<PayTest> payPagingReader() {
+//        return new JpaPagingItemReaderBuilder<PayTest>()
+//                .name("payPagingReader")
+//                .entityManagerFactory(entityManagerFactory)
+//                .pageSize(chunkSize)
+//                .queryString("SELECT pt FROM PayTest pt WHERE pt.successStatus = false")
+//                .build();
+//    }
+
+    //update 하지 않고 insert 한다.
     @Bean
     @StepScope
     public JpaPagingItemReader<PayTest> payPagingReader() {
-        return new JpaPagingItemReaderBuilder<PayTest>()
-                .name("payPagingReader")
-                .entityManagerFactory(entityManagerFactory)
-                .pageSize(chunkSize)
-                .queryString("SELECT pt FROM PayTest pt WHERE pt.successStatus = false")
-                .build();
+
+        JpaPagingItemReader<PayTest> reader = new JpaPagingItemReader<PayTest>() {
+            @Override
+            public int getPage() {
+                return 0;
+            }
+        };
+
+        reader.setQueryString("SELECT p FROM PayTest p WHERE p.successStatus = false");
+        reader.setPageSize(chunkSize);
+        reader.setEntityManagerFactory(entityManagerFactory);
+        reader.setName("payPagingReader");
+
+        return reader;
     }
+
+    /**
+     * 근래 버전 방법
+     * @return
+     */
+//    @Bean
+//    public ItemReader<PayTest> repositoryItemReader() {
+//        return new RepositoryItemReaderBuilder<PayTest>()
+//                .repository(payRepository)
+//                .methodName("findAll")
+//                .pageSize(chunkSize)
+//                .saveState(false)
+//                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+//                .name("payPagingReader")
+//                .build();
+//    }
 
     @Bean
     @StepScope
---
+++ src/test/java/kr/co/imin/batch/job/itemReader/PayCursorJobConfigurationTest.java
@@ -0,0 +1,47 @@
+package kr.co.imin.batch.job.itemReader;
+
+import kr.co.imin.batch.entity.PayTest;
+import kr.co.imin.batch.repository.PayRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBatchTest
+@SpringBootTest(classes = {PayCursorJobConfiguration.class, TestBatchConfig.class})
+//@ContextConfiguration(classes=PayPagingFailJobConfiguration.class)
+@TestPropertySource(properties = {"job.name=" + PayCursorJobConfiguration.JOB_NAME})
+class PayCursorJobConfigurationTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private PayRepository payRepository;
+
+    @Test
+    @DisplayName("같은조건을읽고 업데이트할때")
+    public void equalConditionUpdateTest() throws Exception {
+
+        for (long i = 0; i < 50; i++) {
+            payRepository.save(new PayTest(i, false));
+        }
+
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        //then
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+        assertThat(payRepository.findAllSuccess().size()).isEqualTo(50);
+
+    }
+}
